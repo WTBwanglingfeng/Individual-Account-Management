4b163d267dee5faf55fd9f0956c84649d724b731
WTBwanglingfeng <45139702+WTBwanglingfeng@users.noreply.github.com>
2019-05-04 23:13:01 +0800

    date.h

---
+++ date.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#ifndef __DATE_H__
+#define __DATE_H__
+
+class Date {
+private:
+	int year;
+	int month;
+	int day;
+	int totalDays;
+public:
+	Date(int year = 1, int month = 1, int day = 1);
+	static Date read();
+	int getYear() const { return year; }
+	int getMonth() const { return month; }
+	int getDay() const { return day; }
+	int getMaxDay() const;
+	bool isLeapYear() const {
+		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	}
+	void show() const;
+	int distance(const Date &date) const {
+		return totalDays - date.totalDays;
+	}
+	int operator - (const Date &date) const {
+		return totalDays - date.totalDays;
+	}
+	bool operator < (const Date &date) const {
+		return totalDays < date.totalDays;
+	}
+};
+#endif
